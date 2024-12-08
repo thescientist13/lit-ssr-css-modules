237a48b94249f588c3a3c1793796951e36b231e1
Owen Buckley <owenbuckley13@gmail.com>
2024-12-08 13:05:23 -0500

    basic example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # lit-ssr-css-modules
+
 A demonstration repo of Lit+SSR with CSS Module scripts and Constructable Stylesheets
+
+## Setup
+
+1. Clone the repo
+1. Assumes Node >= 22.x (`nvm use`)
+1. Assumes PNPM (`pnpm i`)
+
+## Demo
+
+The demo simply renders the standard Greeting component from the docs using Lit's SSR package and outputs its contents to _./public/index.html_.
+
+```sh
+$ pnpm run build
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+import fs from 'node:fs/promises';
+import { render, html } from '@lit-labs/ssr';
+import { collectResult } from '@lit-labs/ssr/lib/render-result.js';
+import './components/greeting/greeting.js';
+
+const OUTPUT_DIR = new URL('./public/', import.meta.url);
+const OUTPUT_FILE = new URL('./index.html', OUTPUT_DIR);
+
+// assumes OUTPUT DIR does not already exist
+fs.mkdir(OUTPUT_DIR);
+
+const greetingResult = html`
+  <simple-greeting></simple-greeting>
+  <simple-greeting name="SSR"></simple-greeting>
+`;
+const greetingHtml = await collectResult(render(greetingResult));
+
+console.log({ greetingHtml });
+
+fs.writeFile(OUTPUT_FILE, greetingHtml);
