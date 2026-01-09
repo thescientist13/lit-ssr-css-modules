2aae6e77a362c12034ddd91dc65f894ebc2fae77
Owen Buckley <owenbuckley13@gmail.com>
2026-01-08 20:52:11 -0500

    updating deps and demo

---
+++ register-hook.js
@@ -0,0 +1,24 @@
+// import @lit-labs/ssr-dom-shim/register-css-hook.js
+import '@lit-labs/ssr-dom-shim/register-css-hook.js';
+
+import fs from 'node:fs/promises';
+import { html } from 'lit';
+import { render } from '@lit-labs/ssr';
+import { collectResult } from '@lit-labs/ssr/lib/render-result.js';
+import './components/greeting/greeting.js';
+
+const OUTPUT_DIR = new URL('./public/', import.meta.url);
+const OUTPUT_FILE = new URL('./index.html', OUTPUT_DIR);
+
+// assumes OUTPUT DIR does not already exist
+await fs.mkdir(OUTPUT_DIR);
+
+const greetingResult = html`
+  <simple-greeting></simple-greeting>
+  <simple-greeting name="SSR"></simple-greeting>
+`;
+const greetingHtml = await collectResult(render(greetingResult));
+
+console.log({ greetingHtml });
+
+await fs.writeFile(OUTPUT_FILE, greetingHtml);
